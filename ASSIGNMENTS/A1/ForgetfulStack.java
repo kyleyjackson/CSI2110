3e8609bec6ad4323634f7b0df51ced67014791d3
kyleyjackson <kjackson@bayviewglen.ca>
2025-09-29 17:23:26 -0400

    done a1 code part

--- ASSIGNMENTS/A1/ForgetfulStack.java
+++ ASSIGNMENTS/A1/ForgetfulStack.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class ForgetfulStack<E> {
 
     // nested node class
-    private static class Node<E> {
+    private class Node<E> {
         
         // instance variables
         E element;
@@ -19,13 +19,15 @@ public class ForgetfulStack<E> {
             this.prev = null;
             this.element = data;
         }
+
+        // helper
+        public E getElement() { return element; }
     }
 
     // instance variables
     private Node<E> top;
     private Node<E> bottom;
     private int size;
-    private ArrayList<Node<E>> aux; // reference ArrayList for any instance of ForgetfulStack<E>
 
     // constructor
     public ForgetfulStack() {
@@ -36,17 +38,16 @@ public class ForgetfulStack<E> {
 
     // methods
     public void push(E element) {
-        Node<E> newN = new Node<>(element);
+        Node<E> newN = new Node<>(element); // node to be added
 
         if (top == null) {
             top = bottom = newN;
-        } else {
+        } else { // pointer assignments
             newN.prev = top;
             top.next = newN;
             top = newN;
         }
 
-        aux.add(aux.size() - 1, newN); // add at the back so stack is in order of descending store time
 
         size++;
     }
@@ -54,13 +55,11 @@ public class ForgetfulStack<E> {
     public E pop() {
         if (top == null) { throw new EmptyStackException(); }
 
-        E e = top.element;
-        top = top.prev;
+        E e = top.element; // to be returned
+        top = top.prev; // pointer assignment
         
         if (top == null) { bottom = null; }
         else { top.next = null; }
-
-        aux.remove(0); // always popping top (index 0)
 
         size--;
         
@@ -70,68 +69,82 @@ public class ForgetfulStack<E> {
     public void forget(int k) {
         if (k < 1) { throw new IllegalArgumentException("Can't forget 0 or a negative amount of elements."); } // invalid argument
 
-        if (k >= aux.size()) { // when kthe stack would be empty
-            aux = new ArrayList<>();
+        if (k >= size()) { // empty the stack
             top = bottom = null;
             size = 0;
+        } else {
+            for (int i = 0; i < k; i++) { // constant for all stack sizes per k-value 
+                bottom = bottom.next;
+                bottom.prev = null;
+            }
+
+            size = size - k;
         }
-
-        Node<E> nextToBot = aux.get(k + 1); // grab the soon-to-be second node from the bottom for pointer updates
-
-        // pointer updates
-        bottom = aux.get(k); 
-        bottom.next = nextToBot;
-        nextToBot.prev = bottom;
-
-        // auxiliary list updates
-        aux.set(k + 1, nextToBot);
-        aux.set(k, bottom);
-        aux.subList(k, aux.size());
-
-        size = size - k;
     }
 
     public boolean isEmpty() { return (size == 0); }
 
-    public E top() { return top.element; }
+    public E top() { 
+        if (top == null) { throw new EmptyStackException(); }
+        return top.getElement(); 
+    }
 
     public int size() { return size; }
 
     // NON ASSIGNMENT RELATED
     public String toString() {
-        if (isEmpty()) { return "[]"; }
+        if (size() == 0) { return "Stack (Top -> Bottom): []"; }
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
         Node<E> current = top;
+        String str = "Stack (Top -> Bottom): [";
 
-        while (current != null) {
-            sb.append(current.element);
-
-            if (current.next != null) { sb.append(", "); }
-
-            current = current.next;
+        for (int i = 0; i < size() - 1; i++) {
+            str = str + current.getElement() + ", ";
+            current = current.prev;
         }
 
-        sb.append("]");
-        return sb.toString();
+        str = str + current.getElement() + "]";
+        return str;
     }
 
     // testing
     public static void main(String[] args) {
         ForgetfulStack<Integer> s = new ForgetfulStack<>();
 
-        s.push(1); // [1]
-        s.push(2); // [1, 2]
-        s.push(3); // [1, 2, 3]
-        s.push(4); // [1, 2, 3, 4]
-        s.push(5); // [1, 2, 3, 4, 5]
-        s.push(6); // [1, 2, 3, 4, 5, 6]
+        s.push(1); 
+        s.push(2); 
+        s.push(3); 
+        s.push(4); 
+        s.push(5); 
+        s.push(6); 
+        
+        System.out.println(s.toString()); // [6, 5, 4, 3, 2, 1]
+        System.out.println("Top element: " + s.top()); // 6
+        System.out.println("Size: " + s.size()); // 6
+        System.out.println("Stack is empty: " + s.isEmpty()); // false
+        System.out.println();
 
-        s.pop(); // [1, 2, 3, 4, 5]
+        s.pop(); 
 
-        s.forget(2); // [3, 4, 5]
+        System.out.println(s.toString()); // [5, 4, 3, 2, 1]
+        System.out.println("Top element: " + s.top()); // 5
+        System.out.println("Size: " + s.size()); // 5
+        System.out.println("Stack is empty: " + s.isEmpty()); // false
+        System.out.println();
 
-        s.toString();
+        s.forget(2); 
+
+        System.out.println(s.toString()); // [5, 4, 3]
+        System.out.println("Top element: " + s.top()); // 5
+        System.out.println("Size: " + s.size()); // 3
+        System.out.println("Stack is empty: " + s.isEmpty()); // false
+        System.out.println();
+
+        s.forget(99); 
+
+        System.out.println(s.toString()); // []
+        System.out.println("Size: " + s.size()); // 0
+        System.out.println("Stack is empty: " + s.isEmpty()); // false
+        System.out.println("Top element: " + s.top()); // should throw EmptyStackException
     }
 }
